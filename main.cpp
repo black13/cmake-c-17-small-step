3f328b25730929fe40e98f79555ea2974a36bb61
black_13 <jjosburn@gmail.com>
2023-08-07 11:17:55 -0500

    more complexity

--- main.cpp
+++ main.cpp
@@ -4,17 +4,26 @@
 // Base Expression Template
 struct Expression {
     virtual ~Expression() = default;
-    virtual std::unique_ptr<Expression> step() const = 0; // Define the small-step transition
-    virtual std::unique_ptr<Expression> clone() const = 0; // Define the clone method
-    virtual bool isValue() const { return false; } // Check if it's a final value
+    virtual std::unique_ptr<Expression> step() const = 0;
+    virtual std::unique_ptr<Expression> clone() const = 0;
+    virtual bool isValue() const { return false; }
 };
 
-// Value (final state)
+// Integer Value (final state)
 struct Value : public Expression {
     int value;
     Value(int v) : value(v) {}
     std::unique_ptr<Expression> step() const override { return nullptr; }
     std::unique_ptr<Expression> clone() const override { return std::make_unique<Value>(value); }
+    bool isValue() const override { return true; }
+};
+
+// Boolean Value
+struct BoolValue : public Expression {
+    bool value;
+    BoolValue(bool v) : value(v) {}
+    std::unique_ptr<Expression> step() const override { return nullptr; }
+    std::unique_ptr<Expression> clone() const override { return std::make_unique<BoolValue>(value); }
     bool isValue() const override { return true; }
 };
 
@@ -29,11 +38,36 @@ struct Add : public Expression {
         } else if (!right->isValue()) {
             return std::make_unique<Add>(left->clone(), right->step());
         } else {
-            // Assume left and right are of Value type, perform addition
             return std::make_unique<Value>(static_cast<Value*>(left.get())->value + static_cast<Value*>(right.get())->value);
         }
     }
     std::unique_ptr<Expression> clone() const override { return std::make_unique<Add>(left->clone(), right->clone()); }
+};
+
+// Conditional (Branching) Expression
+struct IfElse : public Expression {
+    std::unique_ptr<Expression> condition;
+    std::unique_ptr<Expression> trueBranch;
+    std::unique_ptr<Expression> falseBranch;
+
+    IfElse(std::unique_ptr<Expression> cond, std::unique_ptr<Expression> tBranch, std::unique_ptr<Expression> fBranch)
+        : condition(std::move(cond)), trueBranch(std::move(tBranch)), falseBranch(std::move(fBranch)) {}
+
+    std::unique_ptr<Expression> step() const override {
+        if (!condition->isValue()) {
+            return std::make_unique<IfElse>(condition->step(), trueBranch->clone(), falseBranch->clone());
+        } else {
+            if (static_cast<BoolValue*>(condition.get())->value) {
+                return trueBranch->clone();
+            } else {
+                return falseBranch->clone();
+            }
+        }
+    }
+
+    std::unique_ptr<Expression> clone() const override {
+        return std::make_unique<IfElse>(condition->clone(), trueBranch->clone(), falseBranch->clone());
+    }
 };
 
 // Function to perform the small-step evaluation
@@ -45,10 +79,11 @@ std::unique_ptr<Expression> evaluate(std::unique_ptr<Expression> expr) {
 }
 
 int main() {
-    // Build an expression for (1 + (2 + 3))
-    auto expr = std::make_unique<Add>(
-        std::make_unique<Value>(1),
-        std::make_unique<Add>(std::make_unique<Value>(2), std::make_unique<Value>(3))
+    // Example: if (true) then 5 else (3 + 4)
+    auto expr = std::make_unique<IfElse>(
+        std::make_unique<BoolValue>(true),
+        std::make_unique<Value>(5),
+        std::make_unique<Add>(std::make_unique<Value>(3), std::make_unique<Value>(4))
     );
 
     // Evaluate the expression
